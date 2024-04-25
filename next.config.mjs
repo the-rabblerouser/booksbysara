f5e9221ece49ef8bc04c9d897394b59708a0a7ca
the-rabblerouser <tojmccann@gmail.com>
2024-04-25 09:56:33 -0500

    meet the author card completed

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// next.config.js
+
+const nextConfig = {
+  // Other Next.js configuration options...
+  head: {
+    // Add or replace any other meta tags as needed
+    link: [
+      {
+        rel: 'icon',
+        href: '/favicon.ico', // Path to your favicon file
+      },
+    ],
+  },
+};
 
 export default nextConfig;
