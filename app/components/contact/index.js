08273bb756603110669ee51d1f5b7617beba984a
the-rabblerouser <tojmccann@gmail.com>
2024-04-26 11:38:05 -0500

    update contact form

--- app/components/contact/index.js
+++ app/components/contact/index.js
@@ -6,44 +6,30 @@ const Contact = () => {
 			<div className='mx-auto mt-[50px] px-4 h-fit'>
 				<div className='flex justify-center items-center w-full mx-auto px-4 rounded-lg overflow-hidden shadow-md p-6 mt-[50px] bg-violet-100'>
 					<form className='md:w-6/12 w-11/12'>
+						<h1 className='text-2xl mb-4'>Contact</h1>
 						<div className='mb-4'>
-							<label
-								htmlFor='name'
-								className='block text-gray-700 font-bold mb-2'>
-								Name
-							</label>
 							<input
 								type='text'
 								id='name'
 								name='name'
-								placeholder='Enter your name'
+								placeholder='Name'
 								className='w-full border border-gray-300 rounded px-4 py-2 focus:outline-none focus:border-blue-500'
 							/>
 						</div>
 						<div className='mb-4'>
-							<label
-								htmlFor='email'
-								className='block text-gray-700 font-bold mb-2'>
-								Email
-							</label>
 							<input
 								type='email'
 								id='email'
 								name='email'
-								placeholder='Enter your email'
+								placeholder='Email'
 								className='w-full border border-gray-300 rounded px-4 py-2 focus:outline-none focus:border-blue-500'
 							/>
 						</div>
 						<div className='mb-4'>
-							<label
-								htmlFor='message'
-								className='block text-gray-700 font-bold mb-2'>
-								Message
-							</label>
 							<textarea
 								id='message'
 								name='message'
-								placeholder='Enter your message'
+								placeholder='Message'
 								rows='4'
 								className='w-full border border-gray-300 rounded px-4 py-2 focus:outline-none focus:border-blue-500'></textarea>
 						</div>
