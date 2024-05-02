8c5eb35df752675f0f6af2ce1bb7f4fae71335f1
the-rabblerouser <tojmccann@gmail.com>
2024-05-01 22:16:49 -0400

    corrected form submit redirect

--- app/actions.js
+++ app/actions.js
@@ -1,9 +1,15 @@
 'use server';
 
+import { redirect } from 'next/navigation';
+import { revalidatePath } from 'next/cache';
+
 export async function handleMyFormSubmit(data) {
 	console.log({
 		name: data.name,
 		email: data.email,
 		message: data.email,
 	});
+
+	revalidatePath('/');
+	redirect(`/}`);
 }
