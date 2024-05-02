c7e650630a639f2b8dc26889051aed8a8e2068d4
the-rabblerouser <tojmccann@gmail.com>
2024-05-01 21:39:08 -0400

    broke out form

--- app/components/contact/index.js
+++ app/components/contact/index.js
@@ -1,64 +1,20 @@
-'use client';
-import React from 'react';
-import { useForm } from 'react-hook-form';
+'use server';
 
-const Contact = () => {
-	const {
-		register,
-		handleSubmit,
-		formState: { errors },
-	} = useForm();
-	const onSubmit = (data) => console.log(data);
+import React from 'react';
+import Form from './form';
+
+const Contact = async () => {
+	async function email(data) {
+		'use server';
+
+		return data;
+	}
 
 	return (
 		<div className='contact_scroll'>
 			<div className='mx-auto mt-[50px] px-4 h-fit'>
 				<div className='flex justify-center items-center w-full mx-auto px-4 rounded-lg overflow-hidden shadow-md p-6 mt-[50px] bg-violet-100'>
-					<form className='md:w-6/12 w-11/12' onSubmit={handleSubmit(onSubmit)}>
-						<h1 className='text-2xl mb-4'>Contact</h1>
-						<div className='mb-4'>
-							<input
-								type='text'
-								id='name'
-								name='name'
-								placeholder='Name'
-								className='w-full border border-gray-300 rounded px-4 py-2 focus:outline-none focus:border-blue-500'
-								{...register('name', { required: true })}
-							/>
-							{errors.name && <span>This field is required</span>}
-						</div>
-						<div className='mb-4'>
-							<input
-								type='email'
-								id='email'
-								name='email'
-								placeholder='Email'
-								className='w-full border border-gray-300 rounded px-4 py-2 focus:outline-none focus:border-blue-500'
-								{...register('email', {
-									required: true,
-									pattern: /^\S+@\S+$/i,
-								})}
-							/>
-							{errors.email && <span>Please enter a valid email</span>}
-						</div>
-						<div className='mb-4'>
-							<textarea
-								id='message'
-								name='message'
-								placeholder='Message'
-								rows='4'
-								className='w-full border border-gray-300 rounded px-4 py-2 focus:outline-none focus:border-blue-500'
-								{...register('message', { required: true })}></textarea>
-							{errors.message && <span>This field is required</span>}
-						</div>
-						<div className='flex justify-center'>
-							<button
-								type='submit'
-								className='border border-gray-300 hover:bg-white font-semibold py-2 px-4 rounded inline-flex items-center'>
-								Submit
-							</button>
-						</div>
-					</form>
+					<Form />
 				</div>
 			</div>
 		</div>
